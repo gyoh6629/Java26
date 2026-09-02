01dc199e32ceb0cebda143b7cb2fc7d0987ad8f3
gyoh6629 <gyoh6629@gmail.com>
2026-09-02 15:56:01 +0900

    Add files via upload

---
+++ day03/Gugudan.java
@@ -0,0 +1,26 @@
+package method;
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		System.out.print("숫자를 입력하세요 : ");
+//		int num = sc.nextInt();
+//		gugudan(num);
+		
+		for(int i = 2; i <= 9; i++) {
+			gugudan(i);
+			System.out.println();
+		}
+	}
+	
+	public static void gugudan(int num) {
+		// 입력된 num의 구구단를 출력
+		System.out.printf("[ 구구단 %d단 ]\n", num);
+		for(int i = 1; i <= 9; i++) {
+			System.out.printf("%d x %d = %d\n", num, i, num * i);
+		}
+	}
+}
