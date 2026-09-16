458d87ce30b1486ad62d49115ebeb47361b295d2
gyoh6629 <gyoh6629@gmail.com>
2026-09-16 17:21:24 +0900

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package anmal;
+
+public class Animal {
+
+	String eye;
+	String mounth;
+	
+	public void eat() {
+		System.out.println("먹이를 먹는다.");
+	}
+	
+	public void sleep() {
+		System.out.println("잠을 잔다.");
+	}
+}
