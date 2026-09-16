a5c209ab0cda0482273b6f7e55f649718fc4e8a3
gyoh6629 <gyoh6629@gmail.com>
2026-09-16 17:23:13 +0900

    Add files via upload

---
+++ day05/BestGirl.java
@@ -0,0 +1,13 @@
+package testbook_chapter06_challenge;
+
+public class BestGirl extends GoodGirl {
+	
+	BestGirl(String name) {
+		super(name);
+	}
+
+	@Override
+	void show() {
+		System.out.println(name + "는 자바를 무지하게 잘 안다.");
+	}
+}
